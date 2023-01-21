a8c7ba72adf842c5846db483fc15b5cfecce9e45
Nandhu <87135537+Nandhu9999@users.noreply.github.com>
2023-01-22 02:32:02 +0530

    Create basic.asm

---
+++ lpc_assembly/basic.asm
@@ -0,0 +1,7 @@
+  AREA     RESET, CODE, READONLY
+                     
+  ENTRY                   
+start
+	MOV R0, #10   		; Move value 10(0x0A) to register 0
+	
+  END             		; end of file
